2f5d0d10dfbe730b546d938b5973bf5096b6e17f
MathisJuretRafin <148776485+MathisJuretRafin@users.noreply.github.com>
2023-12-01 10:46:40 +0100

    Update WomenShop_MySQL.sql

--- WomenShop_MySQL.sql
+++ WomenShop_MySQL.sql
@@ -9,43 +9,57 @@ drop table if exists shoes;
 drop table if exists accessories;
 drop table if exists clothes;
 
-CREATE TABLE `womenshop`.`shoes` (
-`id_shoes` INT NOT NULL AUTO_INCREMENT,
-`name_shoes` VARCHAR(40) NOT NULL,
-`price_shoes` DOUBLE NOT NULL,
-`nbItems_shoes` INT NOT NULL,
-`shoesSize`INT NOT NULL CHECK (`shoesSize`>36 and `shoesSize`<50),
-PRIMARY KEY (`id_shoes`))
+CREATE TABLE `womenshop`.`product`(
+`id` INT NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(40) NOT NULL,
+`price` DOUBLE NOT NULL,
+`purchasePrice`DOUBLE NOT NULL,
+`nbItems` INT NOT NULL,
+primary key(`id`))
 AUTO_INCREMENT = 1;
+
+CREATE TABLE `womenshop`.`shoes` (
+`id` INT NOT NULL,
+`shoesSize`INT NOT NULL CHECK (`shoesSize`>36 and `shoesSize`<50),
+PRIMARY KEY (`id`),
+FOREIGN KEY (`id`) REFERENCES `womenshop`.`product`(`id`));
 
 CREATE TABLE `womenshop`.`accessories` (
-`id_accessories` INT NOT NULL AUTO_INCREMENT,
-`name_accessories` VARCHAR(40) NOT NULL,
-`price_accessories` DOUBLE NOT NULL,
-`nbItems_accessories` INT NOT NULL,
-PRIMARY KEY (`id_accessories`))
-AUTO_INCREMENT = 1;
+`id` INT NOT NULL ,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`id`) REFERENCES `womenshop`.`product`(`id`));
 
 CREATE TABLE `womenshop`.`clothes` (
-`id_clothes` INT NOT NULL AUTO_INCREMENT,
-`name_clothes` VARCHAR(40) NOT NULL,
-`price_clothes` DOUBLE NOT NULL,
-`nbItems_clothes` INT NOT NULL,
+`id` INT NOT NULL,
 `clothesSize`INT NOT NULL CHECK (`clothesSize`>34 and `clothesSize`<54),
-PRIMARY KEY (`id_clothes`))
-AUTO_INCREMENT = 1;
+PRIMARY KEY (`id`),
+FOREIGN KEY (`id`) REFERENCES `womenshop`.`product`(`id`));
 
-INSERT INTO `womenshop`.`clothes`(`name_clothes`, `price_clothes`, `nbItems_clothes`, `clothesSize`) VALUES ('Chemisier', 20.50, 15, 37);
-INSERT INTO `womenshop`.`clothes`(`name_clothes`, `price_clothes`, `nbItems_clothes`, `clothesSize`) VALUES ('T-shirt blanc', 15.75, 10, 39);
-INSERT INTO `womenshop`.`clothes`(`name_clothes`, `price_clothes`, `nbItems_clothes`, `clothesSize`) VALUES ('T-shirt rouge', 7.00, 30, 38);
-INSERT INTO `womenshop`.`clothes`(`name_clothes`, `price_clothes`, `nbItems_clothes`, `clothesSize`) VALUES ('T-shirt noir', 8, 5, 37);
 
-INSERT INTO `womenshop`.`shoes`(`name_shoes`, `price_shoes`, `nbItems_shoes`, `shoesSize`) VALUES ('Baskets', 36.25, 15, 37);
-INSERT INTO `womenshop`.`shoes`(`name_shoes`, `price_shoes`, `nbItems_shoes`, `shoesSize`) VALUES ('Converse', 40.15, 10, 39);
-INSERT INTO `womenshop`.`shoes`(`name_shoes`, `price_shoes`, `nbItems_shoes`, `shoesSize`) VALUES ('Bottes', 67.12, 30, 37);
-INSERT INTO `womenshop`.`shoes`(`name_shoes`, `price_shoes`, `nbItems_shoes`, `shoesSize`) VALUES ('Talons', 30, 5, 38);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('Chemisier', 20.50, 15, 19.5);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('T-shirt blanc', 15.75, 10, 13);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('T-shirt rouge', 7.00, 38,4);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('T-shirt noir', 8, 5,7.5);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('Baskets', 36.25, 15, 35);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('Converse', 40.15, 10, 38);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('Bottes', 67.12, 30,60);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('Talons', 30, 5,25);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('Echarpe', 15, 15, 12);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('Bracelet', 6, 10, 3);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('Boucles d\'oreilles', 30, 30, 25);
+INSERT INTO `womenshop`.`product`(`name`, `price`, `nbItems`, `purchasePrice`) VALUES ('Collier', 100.11, 5, 98);
 
-INSERT INTO `womenshop`.`accessories`(`name_accessories`, `price_accessories`, `nbItems_accessories`) VALUES ('Echarpe', 15, 15);
-INSERT INTO `womenshop`.`accessories`(`name_accessories`, `price_accessories`, `nbItems_accessories`) VALUES ('Bracelet', 6, 10);
-INSERT INTO `womenshop`.`accessories`(`name_accessories`, `price_accessories`, `nbItems_accessories`) VALUES ('Boucles d\'oreilles', 30, 30);
-INSERT INTO `womenshop`.`accessories`(`name_accessories`, `price_accessories`, `nbItems_accessories`) VALUES ('Collier', 100.11, 5);
+INSERT INTO `womenshop`.`clothes`(`id`, `clothesSize`) VALUES (1, 36);
+INSERT INTO `womenshop`.`clothes`(`id`, `clothesSize`) VALUES (2, 37);
+INSERT INTO `womenshop`.`clothes`(`id`, `clothesSize`) VALUES (3, 38);
+INSERT INTO `womenshop`.`clothes`(`id`, `clothesSize`) VALUES (4, 39);
+
+INSERT INTO `womenshop`.`accessories`(`id`) VALUES (9);
+INSERT INTO `womenshop`.`accessories`(`id`) VALUES (10);
+INSERT INTO `womenshop`.`accessories`(`id`) VALUES (11);
+INSERT INTO `womenshop`.`accessories`(`id`) VALUES (12);
+
+INSERT INTO `womenshop`.`shoes`(`id`, `shoesSize`) VALUES (5, 37);
+INSERT INTO `womenshop`.`shoes`(`id`, `shoesSize`) VALUES (6, 38);
+INSERT INTO `womenshop`.`shoes`(`id`, `shoesSize`) VALUES (7, 39);
+INSERT INTO `womenshop`.`shoes`(`id`, `shoesSize`) VALUES (8, 40);
